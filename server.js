620c5392f3d55b05ffed83d958c9566bdd5e5fa1
nlpcloud <tech@babelon.co>
2012-05-29 23:46:15 +0000

    add POST / for fb canvas

--- server.js
+++ server.js
@@ -13,6 +13,10 @@ server.get('/', function(req, res) {
   res.send('<script type="text/javascript" charset="utf-8">window.top.location.href = "http://lietome.babelon.co";</script>');
 });
 
+server.post('/', function(req, res) {
+  res.send('<script type="text/javascript" charset="utf-8">window.top.location.href = "http://lietome.babelon.co";</script>');
+});
+
 port = process.env.PORT || 8080;
 server.listen(port, function() {
   console.log("Listening on " + port);
